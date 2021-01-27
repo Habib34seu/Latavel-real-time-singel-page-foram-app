b876df861d3381578450bb6b8c8865a99fad9d3f
Habibur Rahman <mohammadhabiburrahman5@gmail.com>
2021-01-27 22:45:09 +0600

    Relationship

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Reply;
+use App\Models\Caregory;
 
 class Question extends Model
 {
     use HasFactory;
+
+     public function user(Type $var = null)
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies(Type $var = null)
+    {
+        return $this->hasMany(Reply::class);
+    }
+    public function category(Type $var = null)
+    {
+        return $this->belongsTo(Caregory::class);
+    }
 }
--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Question;
+use App\Models\User;
+use App\Models\Like;
 
 class Reply extends Model
 {
     use HasFactory;
+    public function question(Type $var = null)
+    {
+        return $this->belongsTo(Question::class);
+    }
+    public function user(Type $var = null)
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function likes(Type $var = null)
+    {
+        return $this->hasMany(Like::class);
+    }
+
 }
