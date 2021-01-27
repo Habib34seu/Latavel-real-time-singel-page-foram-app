d567340967d3bb9c336444a71eba47e4d09bc88f
Habibur Rahman <mohammadhabiburrahman5@gmail.com>
2021-01-27 21:48:58 +0600

    Creating Migrations, Model, Controllers, Factories

---
+++ database/factories/CaregoryFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Caregory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CaregoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Caregory::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
