2521cef99778de32a24a557dae9840d349ac69ad
Habibur Rahman <mohammadhabiburrahman5@gmail.com>
2021-01-29 20:58:57 +0600

    Curd Question

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -11,6 +11,14 @@ use App\Models\Caregory;
 class Question extends Model
 {
     use HasFactory;
+    // protected $filable=['title','slug','body','category_id','user_id'];
+
+    protected $guarded=[];
+
+    public function getRouteKeyName(Type $var = null)
+    {
+        return 'slug';
+    }
 
      public function user(Type $var = null)
     {
@@ -24,5 +32,9 @@ class Question extends Model
     public function category(Type $var = null)
     {
         return $this->belongsTo(Caregory::class);
+    }
+    public function getPathAttribute(Type $var = null)
+    {
+        return asset("api/question/$this->slug");
     }
 }
